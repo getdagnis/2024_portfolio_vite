44a5d898e727ee5a40b58ea685da11a46fe9c04c
Dagnis Skurbe <dagnis.skurbe@gmail.com>
2024-04-12 10:24:30 +0300

    project page improvements + hotjar analytics

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -11,19 +11,6 @@ function ProjectPage() {
   return (
     <div id="project-container">
       <div id="project-grid">
-        <div id="project-nav">
-          {projects.map((p, index) => (
-            <Link
-              to={`/project/${p.key}`}
-              key={p.key}
-              className={p.key === params.key ? 'proj-thumb active' : 'proj-thumb'}
-              style={{
-                backgroundImage: `url(../thumbs/${p.key}.svg)`,
-                animationDelay: `${0.3 + index / 25}s`, // apply bounce animation delay
-              }}
-            ></Link>
-          ))}
-        </div>
         <div id="project-details">
           <div className="proj-head">
             {false && <img className="proj-details-thumb" src={`../thumbs/${params.key}.svg`}></img>}
@@ -38,6 +25,19 @@ function ProjectPage() {
             <img src={`../../proj-img/${proj.key}/${proj.images[0]}`} alt={proj.name} className="main-img" />
             <p className="description">{proj.description}</p>
           </div>
+        </div>
+        <div id="project-nav">
+          {projects.map((p, index) => (
+            <Link
+              to={`/project/${p.key}`}
+              key={p.key}
+              className={p.key === params.key ? 'proj-thumb active' : 'proj-thumb'}
+              style={{
+                backgroundImage: `url(../thumbs/${p.key}.svg)`,
+                animationDelay: `${0.3 + index / 25}s`, // apply bounce animation delay
+              }}
+            ></Link>
+          ))}
         </div>
       </div>
     </div>
