2dc668ebfb6d78a04b19a54850eac8dc7471c405
Dagnis Skurbe <dagnis.skurbe@gmail.com>
2024-09-20 21:26:40 +0300

    added google analytics react library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+
 import Header from './layout/Header';
 import Footer from './layout/Footer';
-
 import './App.css';
 
+// Google Analytics
+const TRACKING_ID = 'G-JWRXKK1QPP';
+ReactGA.initialize(TRACKING_ID);
+
 function App({ children }) {
+  useEffect(() => {
+    ReactGA.send('pageview');
+  }, []);
+
   return (
     <>
       <div id="site-container">
